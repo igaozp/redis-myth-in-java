fd993ca827fb65730b850c0b9bdb2755a727eeb1
igaozp <igaozp@hotmail.com>
2022-05-06 21:18:45 +0800

    Init commit

---
+++ src/main/java/xyz/andornot/redismyth/RedisMythApplication.java
@@ -0,0 +1,13 @@
+package xyz.andornot.redismyth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMythApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMythApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/andornot/redismyth/RedisMythApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.andornot.redismyth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisMythApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
